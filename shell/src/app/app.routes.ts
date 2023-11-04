382663d25ea075e370955355610f7fb6e607f0ef
Sinan Kara <snan_kara_@hotmail.com>
2023-11-04 20:08:34 +0300

    dashboard, nav and menu components created. Primeng theme applied.

--- shell/src/app/app.routes.ts
+++ shell/src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Route } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const appRoutes: Route[] = [
-  {},
+  {
+    path: '', component: DashboardComponent
+  },
 ];
---
+++ shell/src/app/shared/nav/nav.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'pms-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+
+export class NavComponent {
+
+  @Input() topMenuActive!: boolean;
+  
+  @Output() toggleMenuHandleEvent = new EventEmitter<Event>();
+  
+  @Output() toggleTopMenuHandleEvent = new EventEmitter<Event>();
+
+  toggleMenuHandle(event: Event){
+    this.toggleMenuHandleEvent.emit(event);
+  }
+
+  toggleTopMenuHandle(event: Event){
+    this.toggleTopMenuHandleEvent.emit(event);
+  }
+}
