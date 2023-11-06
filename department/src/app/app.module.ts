b7e221ec64a4918b42b478a1bb8f13ea2453a729
Sinan Kara <snan_kara_@hotmail.com>
2023-11-06 23:08:18 +0300

    shared styles library edited.

--- department/src/app/app.module.ts
+++ department/src/app/app.module.ts
@@ -2,16 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
-import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    /*Only mfe*/
-    BrowserModule,
-    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' })
-
-    //RouterModule.forChild(appRoutes)
+    //Independent start
+    //RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' })
+    
+    RouterModule.forChild(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent],
